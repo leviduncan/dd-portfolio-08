47bc4d6104d7f58194fe410f0e26a239cdb36bd9
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-08-18 00:05:20 -0400

    Update Hero and About sections

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import Darrin from '../../assets/darrinPhoto.png'
 
 function About() {
   return (
     <section id="about">
     <div className="container about">
-        <div className="about-heading">
-            <h2>About</h2>
+        <div className="about-heading text-center">
+            <h2>Hi, I'm Darrin Duncan</h2>
+            <h3>A Super Frontend Developer</h3>
         </div>
-        <div className="about-content">
-            
+        <div className="about-content d-flex mx-auto mt-4 flex-column">
+
+            <div className="about-content-left col-12 col-md-7 mx-auto my-4">
+                <img src={Darrin} alt="About Darrin" />
+            </div>
+            <div className="about-content-right col-12 col-md-8 mx-auto mx-md-6">
             <p>
             As a Web Developer, I often gather, define and interpret business requirements, to design, build, enhance, test, and deploy modern web applications. I use code to solve problems. Whether working independently or within a team setting, I am able to juggle multiple projects while managing quality, communication, and creativity. I am always available to chat with you about your next project or current one. So let's connect! I look forward to speaking with you.
             </p>
+            </div>
         </div>
         <div className="reach-out">
 
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Hero() {
     return (
         <section id="hero">
+            <div className="shape-org"></div>
             <div className="container hero">
                 <div className="hero-content-left">
-                    <h1><span className="text-orange">Powerful</span> Frontend Development</h1>
+                    <h1><div className="text-orange">Powerful</div> Frontend Development</h1>
                 </div>
                 <div className="hero-content-right">
                     <h4>
                         Building Engaging Front-end Experiences.
                     </h4>
+                </div>
+                <div>
+                    <Link to="#contact" className="btn btn-light btn-lg col-12 col-md-6 col-lg-3 my-4">Let's Connect</Link>
                 </div>
             </div>
         </section>
