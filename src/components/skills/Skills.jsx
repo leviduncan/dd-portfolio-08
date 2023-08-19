75497d53864f35a76ed3866bfa100d29fca8b089
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-08-19 15:36:33 -0400

    Update skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -10,11 +10,11 @@ function Skills() {
 
     return (
         <section id="skills">
-            
             <div className="container skills">
                 <div className="skills-heading">
                     <h2>My Skills</h2>
                 </div>
+                    <p>As a problem solver I build fullstack web applications using technology like HTML, CSS, JavaScript, React JS and MongoDB. I have highlighted some of the key technical skills that I will bring to your project.</p>
                 <div className="skills-content">
                     {data.map((item, idx) => (
                         <h4 key={idx}>{item.name}</h4>
